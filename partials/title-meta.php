9719bce1b62437f56e102e083606cd6be54ad2fe
webcreation-dev <adjilan2403@gmail.com>
2025-04-04 21:47:17 +0100

    edit title

--- partials/title-meta.php
+++ partials/title-meta.php
@@ -1,6 +1,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ovro - Personal Portfolio PHP Template</title>
+  <title>Esmeralda AGBESSY l Développeur Frontend Web et Mobile & Designer d'interfaces UI/UX</title>
 
   <!--=====FAB ICON=======-->
   <link rel="shortcut icon" href="assets/img/logo/fav-logo1.png" type="image/x-icon">
