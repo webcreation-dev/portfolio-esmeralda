3550c439df40567494920f9612bcdf1f0a31535c
webcreation-dev <adjilan2403@gmail.com>
2025-04-07 00:01:57 +0100

    maj

--- index6.php
+++ index6.php
@@ -95,7 +95,7 @@
               </div>
               <div class="space32"></div>
               <div class="main-btn-area" data-aos="fade-up" data-aos-duration="1200">
-                <a href="#">Digitalisons ensemble votre entreprise ! <span><i class="fa-solid fa-arrow-right"></i></span></a>
+                <a href="#">Digitalisons ensemble ! <span><i class="fa-solid fa-arrow-right"></i></span></a>
               </div>
             </div>
           </div>
